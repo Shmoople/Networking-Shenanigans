ebea4cd77538cf890a8c4a74151971f0e845d593
Wyatt Rose <94425062+Shmoople@users.noreply.github.com>
2021-12-16 07:49:03 -0500

    Update Client.java

--- messenger/Client.java
+++ messenger/Client.java
@@ -2,55 +2,60 @@
 // Java implementation for multithreaded chat client
 // Save file as Client.java
 
-import java.io.*;
-import java.net.*;
-import java.util.Scanner;
+import java.io.*; // used for data streams
+import java.net.*; // used for sockets and IP address
+import java.util.Scanner; // used for command line input
 
+// client class should be run after server, that way a connection can be established (this version of client doesn't wait)
 public class Client
 {
+    // the current port that the server is on, since this is all done on the localhost it makes it a lot easier
     final static int ServerPort = Server.port;
 
+    // main method is called in parallel with the 'Server' main method
     public static void main(String[] args) throws UnknownHostException, IOException
     {
+        // initialize a new scanner for command-line input
         Scanner in = new Scanner(System.in);
 
-        // getting localhost ip
+        // get the ip of the localhost, this usually defaults to 127.0.0.1
         InetAddress ip = InetAddress.getByName("localhost");
 
-        // establish the connection
+        // start a new connection on the specified port (in this case the port is defined in 'Server'
         Socket s = new Socket(ip, ServerPort);
 
-        // obtaining input and out streams
+        // get the input and output streams from the new connection (sockets return these streams)
         DataInputStream input = new DataInputStream(s.getInputStream());
         DataOutputStream output = new DataOutputStream(s.getOutputStream());
 
-        // sendMessage thread
+        // sendMessage thread, (this thread is polling at the same time as the readMessage thread)
         new Thread(()-> {
             while (true) {
 
-                // read the message to deliver.
+                // read input from the command-line, this is the input from the client-side
                 String msg = in.nextLine();
                 try {
-                    // write on the output stream
+                    // write the message to the output stream, if the connection gets disrupted the program terminates
                     output.writeUTF(msg);
                 } catch (IOException e) {
-                    // do nothing for now...
+                    // unhandled exception, could add recovery later
                 }
             }
-        }).start();
+        }).start(); // start the thread
 
         // readMessage thread
         new Thread(()-> {
             while (true) {
                 try {
-                    // read the message sent to this client
+                    // read the messages from the input stream
                     String msg = input.readUTF();
+                    
+                    // display the message
                     System.out.println(msg);
                 } catch (IOException e) {
                     // do nothing for now...
                 }
             }
-        }).start();
-
+        }).start(); // start the thread
     }
 }
